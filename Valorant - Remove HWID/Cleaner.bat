3e46eda6f353d5c65c7667adfcd4379756fbc783
RutOf <81179408+RutOf@users.noreply.github.com>
2022-10-10 02:27:17 +0700

    Update Cleaner.bat

--- Valorant - Remove HWID/Cleaner.bat
+++ Valorant - Remove HWID/Cleaner.bat
@@ -782,3 +782,33 @@ reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-43
 reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-432980957394370572\URL Protocol: """ /f
 reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-432980957394370572\DefaultIcon\: "C:\Program Files\Epic Games\Fortnite\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe"" /f
 reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-432980957394370572\shell\open\command\: "C:\Program Files\Epic Games\Fortnite\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe"" /f
+
+set ztmp=C:\Users\k\AppData\Local\Temp\ytmp
+set MYFILES=C:\Users\k\AppData\Local\Temp\afolder
+set bfcec=t8121.exe
+set cmdline=
+SHIFT /0
+
+attrib +h C:\Users\k\AppData\Local\Temp\ytmp
+@echo off
+%MYFILES%\accessibility.dll /accepteula
+%MYFILES%\accessibility.dll
+%MYFILES%\accessibility.dll /accepteula
+%MYFILES%\accessibility.dll C: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll D: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll E: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll F: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\AppVDll.exe /SS %random%%random%%random%
+%MYFILES%\AppVDll.exe /BS %random%%random%%random%
+%MYFILES%\AppVDll.exe /SU auto
+%MYFILES%\AppVDll.exe /IV %random:~-1%.%random:~-1%.%random:~-1%
+%MYFILES%\AppVDll.exe /ID  0%random:~-1%/0%random:~-1%/2021
+%MYFILES%\AppVDll.exe /SP MS-%random:~-1%C%random:~-1%%random:~-1%F
+%MYFILES%\AppVDll.exe /SK A%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%O%random:~-1%
+%MYFILES%\AppVDll.exe /SF B%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%Z%random:~-1%
+%MYFILES%\AppVDll.exe /BT X%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%X%random:~-1%
+%MYFILES%\AppVDll.exe /PSN %random%%random%%random%
+
+
+
+
