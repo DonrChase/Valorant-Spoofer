c20fdb23a7342d5e31b82b11a8f75d93f0f538af
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-15 23:59:44 +0700

    Update Valorant2.bat

--- Valorant - Remove HWID/Valorant2.bat
+++ Valorant - Remove HWID/Valorant2.bat
@@ -782,3 +782,32 @@ reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-43
 reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-432980957394370572\URL Protocol: """ /f
 reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-432980957394370572\DefaultIcon\: "C:\Program Files\Epic Games\Fortnite\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe"" /f
 reg delete "HKU\S-1-5-21-2532382528-581214834-2534474248-1001_Classes\discord-432980957394370572\shell\open\command\: "C:\Program Files\Epic Games\Fortnite\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe"" /f
+echo your shitty BaseBoard
+wmic baseboard get serialnumber
+echo your shitty Bios
+wmic bios get serialnumber
+echo your shitty Cpu
+wmic cpu get serialnumber
+echo your shitty DiskDrive (#1) C:
+wmic diskdrive get serialnumber
+echo Diskdrive (#2)
+wmic path win32_physicalmedia get SerialNumber
+echo Diskdrive (#3)
+wmic path win32_diskdrive get SerialNumber
+echo your shitty BaseBoard
+wmic baseboard get manufacturer
+echo RAM
+wmic memorychip get serialnumber
+echo CPU
+wmic cpu get processorid
+echo BaseBoard
+wmic baseboard get manufacturer
+echo GPU
+wmic PATH Win32_VideoController GET Description,PNPDeviceID
+echo Mac Address
+getmac
+echo Other Stuff
+AMIDEWIN.EXE /dms serials.txt
+type serials.txt
+del /f serials.txt
+exit
