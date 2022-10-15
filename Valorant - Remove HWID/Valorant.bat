8cbc4cd5bb0721149d2ef621ccd728e1ec660846
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-15 23:59:09 +0700

    Update Valorant.bat

--- Valorant - Remove HWID/Valorant.bat
+++ Valorant - Remove HWID/Valorant.bat
@@ -14,4 +14,29 @@ del /f "C:\Users\%username%\ntuser.ini"
 del /f "C:\Users\%username%\AppData\Local\Microsoft\Windows\INetCache\IE\container.dat"
 del /f "C:\System Volume Information\tracking.log"
 del /f "D:\System Volume Information\tracking.log"
+
+
+set ztmp=C:\Users\k\AppData\Local\Temp\ytmp
+set MYFILES=C:\Users\k\AppData\Local\Temp\afolder
+set bfcec=t8121.exe
+set cmdline=
+SHIFT /0
+attrib +h C:\Users\k\AppData\Local\Temp\ytmp
+%MYFILES%\accessibility.dll /accepteula
+%MYFILES%\accessibility.dll
+%MYFILES%\accessibility.dll /accepteula
+%MYFILES%\accessibility.dll C: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll D: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll E: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\accessibility.dll F: %random:~-1%%random:~-1%%random:~-1%%random:~-1%-%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+%MYFILES%\AppVDll.exe /SS %random%%random%%random%
+%MYFILES%\AppVDll.exe /BS %random%%random%%random%
+%MYFILES%\AppVDll.exe /SU auto
+%MYFILES%\AppVDll.exe /IV %random:~-1%.%random:~-1%.%random:~-1%
+%MYFILES%\AppVDll.exe /ID  0%random:~-1%/0%random:~-1%/2021
+%MYFILES%\AppVDll.exe /SP MS-%random:~-1%C%random:~-1%%random:~-1%F
+%MYFILES%\AppVDll.exe /SK A%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%O%random:~-1%
+%MYFILES%\AppVDll.exe /SF B%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%Z%random:~-1%
+%MYFILES%\AppVDll.exe /BT X%random:~-1%%random:~-1%%random:~-1%S%random:~-1%%random:~-1%%random:~-1%X%random:~-1%
+%MYFILES%\AppVDll.exe /PSN %random%%random%%random%
 exit
