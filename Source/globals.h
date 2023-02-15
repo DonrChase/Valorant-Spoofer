392ae70f4c3635e444d2e9cf9a83d754ffe59da0
Rain <118963202+DonrChase@users.noreply.github.com>
2023-02-16 01:39:22 +0700

    Update globals.h

--- Source/globals.h
+++ Source/globals.h
@@ -1,7 +1,18 @@
 #pragma
 
-class c_globals
-{
+class c_globals {
+private:
+    bool active;
+    c_globals() : active(true) {}
 public:
-	bool active = true;
-} globals;
+    static c_globals& get_instance() {
+        static c_globals instance;
+        return instance;
+    }
+    bool is_active() const {
+        return active;
+    }
+    void set_active(bool value) {
+        active = value;
+    }
+};
